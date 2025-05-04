705a09091d2dcc718f27b2821a7030cc25e85e56
MariamGed <mariamgedenidze13@gmail.com>
2025-05-04 00:17:42 -0400

    update visualisation

--- code/multi_outcomes_synth_V0.2.r
+++ code/multi_outcomes_synth_V0.2.r
@@ -6,7 +6,7 @@ library(augsynth)
 # Load the data
 setwd("/Users/mariamgedenidze/Desktop/YSE Thesis/master_thesis/")
 #data <- read.csv("data/Maisa_single_treated_annual/Maisa_2001_2019_mean_annual_V3.csv")
-data <- read.csv("data/Maisa_single_treated_annual/Maisa_2001_2020_annual_full_V1.csv")
+data <- read.csv("data/Maisa_single_treated_annual/Maisa_2001_2020_annual_full_V2.csv")
 
 # ---- Data cleaning and formatting ----
 
@@ -22,7 +22,7 @@ data <- data[order(data$geometry_name, data$year), ]
 
 # Drop column .geo 
 data$.geo <- NULL
-
+data$system.index <- NULL
 #View(data)
 # Check for missing values
 sum(is.na(data))
@@ -38,22 +38,70 @@ data <- data %>% filter(year != 2013)
 # Drop donor 5 and 60 all together
 data <- data %>% filter(!(geometry_name %in% c("donor5.0", "donor60.0")))
 
+# Drop outliers, 1.5 std deviation from the mean (Introduces missing values )
+#data <- data %>% filter(deforestation < mean(data$deforestation, na.rm = TRUE) + 1.5 * sd(data$deforestation, na.rm = TRUE))
+#data <- data %>% filter(deforestation > mean(data$deforestation, na.rm = TRUE) - 1.5 * sd(data$deforestation, na.rm = TRUE))
 
-syn_NDVI <- augsynth(deforestation + NDVI ~ treatment, geometry_name, year, data, progfunc = 'None', scm=T) # multiple treated points, so running multisynth
-summary(syn_NDVI)
-plot(syn_NDVI)
+# --- Standardise the data ----
+# Standardise the outcome variables
+#data$deforestation <- (data$deforestation - mean(data$deforestation, na.rm = TRUE)) / sd(data$deforestation, na.rm = TRUE)
+#data$NDVI <- (data$NDVI - mean(data$NDVI, na.rm = TRUE)) / sd(data$NDVI, na.rm = TRUE)
+#mean(data$NDVI, na.rm = TRUE) # Checking that the mean is ~0
 
-# using gsynth instead
+# Standardise the covariates
+#data$SR_B1 <- (data$SR_B1 - mean(data$SR_B1, na.rm = TRUE)) / sd(data$SR_B1, na.rm = TRUE)
+#data$SR_B2 <- (data$SR_B2 - mean(data$SR_B2, na.rm = TRUE)) / sd(data$SR_B2, na.rm = TRUE)
+#data$SR_B5 <- (data$SR_B5 - mean(data$SR_B5, na.rm = TRUE)) / sd(data$SR_B5, na.rm = TRUE)
+#data$SR_B7 <- (data$SR_B7 - mean(data$SR_B7, na.rm = TRUE)) / sd(data$SR_B7, na.rm = TRUE)
+# View the data
+
+# ---- Running single outcome synth control ----
+# Synth control augmented with Ridge regression
+syn_deforestation <- augsynth(deforestation ~ treatment | SR_B1 + SR_B2 + SR_B7, geometry_name, year, data, progfunc = 'Ridge', scm=T) # multiple treated points, so running multisynth
+plot(syn_deforestation, inf_type = "jackknife+") # Pointwise confidence interval)
+summary(syn_deforestation) # Two-tail hypothesis test
+summary(syn_deforestation, stat_func = function(x) -sum(x)) # One-tail hypothesis test against positive effects)
+summary(syn_deforestation, stat_func = function(x) abs(x)) # One-tail test for the average post-treatment effect
+
+plot(syn_deforestation, cv = T)
+
+
+syn_deforestation <- augsynth(deforestation ~ treatment, geometry_name, year, data, progfunc = 'None', scm=T) # multiple treated points, so running multisynth
+summary(syn_deforestation)
+plot(syn_deforestation, inf_type = "jackknife+") # Pointwise confidence interval)
+
+
+# ---- Running multiple outcome synth control ---- 
+syn_multi <- augsynth(deforestation + SR_B7 + SR_B1 + SR_B2 + NDVI ~ treatment, geometry_name, year, data, progfunc = 'None', scm=T)
+summary(syn_multi)
+plot(syn_multi)
+
+# export the weights
+weights <- syn_multi$w
+weights <- as.data.frame(weights)
+colnames(weights) <- c("weights")
+weights$geometry_name <- rownames(weights)
+# save the weights
+write.csv(weights, "data/multi_outcomes_weightsV1.csv", row.names = FALSE)
+
+# ---- using gsynth instead ----
 library(gsynth) # For generalised synth control 
 library(panelView) # For visualising panel data
 
-panelview(deforestation ~ treatment, data = data, index = c("geometry_name", "year"), pre.post = TRUE)
+# Take subset of 10 donors + treatment
+data_subset <- data %>% filter(geometry_name %in% c("donor1.0", "donor2.0", "donor3.0", "donor4.0", "donor5.0", "donor6.0", "donor7.0", "donor8.0", "donor9.0", "donor10.0", "treated"))
+
+panelview(deforestation ~ treatment, data = data_subset, index = c("geometry_name", "year"), pre.post = TRUE, ylab = "Controls", collapse.history = "TRUE")
 panelview(NDVI ~ treatment, data = data, index = c("geometry_name", "year"), type = "outcome")
+
+# full data panel_view
+panelview(deforestation ~ treatment, data = data, index = c("geometry_name", "year"), pre.post = TRUE, ylab = "Controls")
+panelview(deforestation ~ treatment, data = data, index = c("geometry_name", "year"), type = "outcome")
 
 
 # Run gsynth
 system.time(
-    out <- gsynth(deforestation ~ treatment + SR_B1 + SR_B2 + SR_B5 + SR_B7, data = data,
+    out <- gsynth(deforestation ~ treatment, data = data,
                   index = c("geometry_name","year"), force = "two-way",
                   CV = TRUE, r = c(0, 3), se = TRUE, 
                   inference = "parametric", nboots = 1000, 
@@ -61,8 +109,24 @@ system.time(
 )
 
 print(out)
+plot(out)
 out$est.avg
 out$est.beta
 plot(out, type = "counterfactual", raw = "none", main="")
 plot(out, type = "counterfactual", raw = "all")
+plot(out, type = "counterfactual", raw = "band", 
+     xlab = "Time")#, ylim = c(-30,25))
 
+# Plot the distribution of $deforestation of non-treated units
+library(ggplot2)
+
+ggplot(data, aes(x = deforestation)) +
+    geom_histogram(binwidth = 0.01, fill = "blue", color = "black", alpha = 0.7) +
+    labs(title = "Distribution of Deforestation in Non-Treated Units",
+         x = "Deforestation",
+         y = "Frequency") +
+    theme_minimal()
+
+# Find outliers of deforestation
+outliers <- boxplot.stats(data$deforestation)$out
+outliers
---
+++ code/visualisations.r
@@ -0,0 +1,69 @@
+
+
+data <- read.csv("data/Maisa_single_treated_annual/Maisa_2001_2020_annual_full_V2.csv")
+weights <- read.csv("data/multi_outcomes_weightsV1.csv")
+# add one row, whith weights[treated, treated] = 
+weights <- rbind(weights, c(0, "treated"))
+weights$weights <- as.numeric(weights$weights)
+
+# merge the weights with the data
+data <- merge(data, weights, by = "geometry_name")
+# keep only: geometry_name, centroid_lat, centroid_lon, weights
+data <- data[, c("geometry_name", "centroid_lat", "centroid_lon", "weights")]
+# keep only unique geometry_name
+data <- data[!duplicated(data$geometry_name), ]
+View(data)
+
+# histrogram of weights
+hist(weights$weights.y, breaks = 100, main = "Histogram of Weights", xlab = "Weights", ylab = "Frequency")
+# Drop those with weights < 0.01
+data_sub$weights <- as.numeric(data_sub$weights)
+data_sub <- data[data$weights > 0.001, ]
+hist(data_sub$weights, breaks = 100, main = "Histogram of Weights", xlab = "Weights", ylab = "Frequency")
+# ---- Visualisations ----
+library(sf)
+library(ggplot2)
+library(viridis)
+library(dplyr)
+library(ggspatial)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(geobr) # For brazilian borders
+
+
+df <- data_sub
+df$weights <- as.numeric(df$weights)
+# Convert to sf object
+df_sf <- st_as_sf(df, coords = c("centroid_lon", "centroid_lat"), crs = 4326)
+
+# Extract treated point
+treated_point <- df_sf %>% filter(geometry_name == "treated") %>% st_geometry() %>% .[[1]]
+
+# Create lines from treated to each other point
+lines_list <- df_sf %>%
+  filter(geometry_name != "treated") %>%
+  rowwise() %>%
+  mutate(
+    line = st_sfc(st_linestring(rbind(st_coordinates(treated_point), st_coordinates(geometry))), crs = 4326)
+  )
+
+# Create an sf object of lines with associated weights
+lines_sf <- st_sf(weight = lines_list$weights, geometry = st_sfc(lines_list$line))
+
+# Get a basemap using rnaturalearth (world map)
+world <- ne_countries(scale = "medium", returnclass = "sf")
+#  Load Pará state polygon
+para <- read_state(code_state = "PA", year = 2020)
+belem <- read_municipality(code_muni = "Belém", year = 2020)
+# Plotting
+ggplot() +
+  geom_sf(data = belem, fill = "gray95", color = "gray80") +
+  geom_sf(data = lines_sf, aes(color = weight), size = 1) +
+  geom_sf(data = df_sf %>% filter(geometry_name != "treated"), color = "black", size = 2) +
+  geom_sf(data = df_sf %>% filter(geometry_name == "treated"), color = "red", size = 2) +
+  scale_color_viridis(name = "Weight", option = "D") +
+  annotation_scale(location = "bl", width_hint = 0.3) +
+  annotation_north_arrow(location = "tl", which_north = "true", style = north_arrow_minimal()) +
+  coord_sf(xlim = st_bbox(para)[c("xmin", "xmax")], ylim = st_bbox(para)[c("ymin", "ymax")]) +
+  theme_minimal() +
+  ggtitle("Connections to Treated Point in Pará, Brazil")
